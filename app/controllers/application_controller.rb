c3615bb11d621f6418bdd45f8b7c69d1028b72e8
kondohitsuki1205-labo <kondo.itsuki1205@gmail.com>
2025-11-07 14:35:54 +0900

    chore(layout,auth): add shared/_flash, cleanup items view to use layout, and fix basic_auth guard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,20 @@
 class ApplicationController < ActionController::Base
+  # 本番 or ENVでONのときだけBasic認証を有効化
   before_action :basic_auth, if: :basic_auth_enabled?
 
   private
 
   def basic_auth_enabled?
-    return false if Rails.env.test?
-
-    ENV['ENABLE_BASIC_AUTH'].present?
-    + ENV['ENABLE_BASIC_AUTH'].present? &&
-      +     ENV['BASIC_AUTH_USER'].present? &&
-      +     ENV['BASIC_AUTH_PASSWORD'].present?
+    enabled_flag = ENV["ENABLE_BASIC_AUTH"] == "true"
+    (Rails.env.production? || enabled_flag) &&
+      ENV["BASIC_AUTH_USER"].present? &&
+      ENV["BASIC_AUTH_PASSWORD"].present?
   end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |user, pass|
-      -     ActiveSupport::SecurityUtils.secure_compare(user.to_s, ENV['BASIC_AUTH_USER'].to_s) &&
-        - ActiveSupport::SecurityUtils.secure_compare(pass.to_s, ENV['BASIC_AUTH_PASSWORD'].to_s)
-      +     ActiveSupport::SecurityUtils.secure_compare(user.to_s, ENV['BASIC_AUTH_USER'].to_s) &&
-        + ActiveSupport::SecurityUtils.secure_compare(pass.to_s, ENV['BASIC_AUTH_PASSWORD'].to_s)
+      ActiveSupport::SecurityUtils.secure_compare(user, ENV["BASIC_AUTH_USER"]) &
+        ActiveSupport::SecurityUtils.secure_compare(pass, ENV["BASIC_AUTH_PASSWORD"])
     end
-  end
-
-  def configure_permitted_parameters
-    added = %i[
-      nickname
-      last_name first_name
-      last_name_kana first_name_kana
-      birth_date
-    ]
-    devise_parameter_sanitizer.permit(:sign_up, keys: added)
-    devise_parameter_sanitizer.permit(:account_update, keys: added)
   end
 end
