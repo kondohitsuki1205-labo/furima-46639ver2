f40fd5ecd01633b0e00f51d36c049a44f4d6e732
kondohitsuki1205-labo <kondo.itsuki1205@gmail.com>
2025-11-07 15:09:01 +0900

    feat(devise): complete user registration flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   # 本番 or ENVでONのときだけBasic認証を有効化
   before_action :basic_auth, if: :basic_auth_enabled?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    keys = %i[nickname last_name first_name last_name_kana first_name_kana birth_date]
+    devise_parameter_sanitizer.permit(:sign_up,        keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+  end
 
   private
 
