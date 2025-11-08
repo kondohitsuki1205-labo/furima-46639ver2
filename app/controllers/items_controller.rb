211c6ca60051e88399811a680d9c4be4541f3c6e
kondohitsuki1205-labo <kondo.itsuki1205@gmail.com>
2025-11-08 15:58:25 +0900

    fix(items/index): comment out show link until implemented and show shipping fee name

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @items = Item.includes(image_attachment: :blob).order(created_at: :desc)
   end
 
   def new
@@ -13,13 +14,12 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      flash.now[:alert] = '入力内容を確認してください。'
+      flash.now[:alert] = "入力内容を確認してください。"
       render :new, status: :unprocessable_entity
     end
   end
 
   private
-
   def item_params
     params.require(:item).permit(
       :image, :name, :description, :category_id, :condition_id,
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root "items#index"
-  resources :items, only: [:new, :create]
+  resources :items, only: [:index, :new, :create]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
