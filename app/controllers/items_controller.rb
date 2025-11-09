2e8d25095b65961476048ea0280a8e32e2aee22a
kondohitsuki1205-labo <kondo.itsuki1205@gmail.com>
2025-11-09 16:16:37 +0900

    feat(items/edit): add product edit form (keeps image if unchanged)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,13 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :author_only, only: [:edit, :update]
 
   def index
     @items = Item.includes(image_attachment: :blob).order(created_at: :desc)
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @item = Item.new
@@ -18,14 +18,33 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      flash.now[:alert] = '入力内容を確認してください。'
+      flash.now[:alert] = "入力内容を確認してください。"
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    filtered_params = item_params
+    filtered_params.delete(:image) if params.dig(:item, :image).blank?
+
+    if @item.update(filtered_params)
+      redirect_to item_path(@item)
+    else
+      flash.now[:alert] = "入力内容を確認してください。"
+      render :edit, status: :unprocessable_entity
     end
   end
 
   private
 
   def set_item = @item = Item.find(params[:id])
+
+  def author_only
+    redirect_to root_path unless current_user == @item.user
+  end
 
   def item_params
     params.require(:item).permit(
@@ -34,3 +53,4 @@ class ItemsController < ApplicationController
     ).merge(user_id: current_user.id)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root "items#index"
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
